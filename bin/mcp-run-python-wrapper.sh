346dba81d025b37e82b054267833eca0a7e3b0f8
Joshua Shew <joshua.t.shew@gmail.com>
2025-04-18 10:10:33 -0400

    Moved mcp-run-python's working directory out of ~

--- bin/mcp-run-python-wrapper.sh
+++ bin/mcp-run-python-wrapper.sh
@@ -3,5 +3,6 @@
 # This is a wrapper script to change the working directory before starting
 # MCP Run Python with `deno`
 
-cd /Users/Joshua
+mkdir -p /Users/Joshua/.dotfiles/.untracked/mcp-run-python
+cd /Users/Joshua/.dotfiles/.untracked/mcp-run-python
 /Users/Joshua/.deno/bin/deno run -N -R=node_modules -W=node_modules --node-modules-dir=auto jsr:@pydantic/mcp-run-python stdio
