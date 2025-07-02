f24b21db74761e9fa8ccbf674804389de978e1f2
Joshua Shew <joshua.t.shew@gmail.com>
2025-07-01 22:21:08 -0400

    Removed the run python tool

--- bin/mcp-run-python-wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This is a wrapper script to change the working directory before starting
-# MCP Run Python with `deno`
-
-mkdir -p /Users/Joshua/.dotfiles/.untracked/mcp-run-python
-cd /Users/Joshua/.dotfiles/.untracked/mcp-run-python
-/Users/Joshua/.deno/bin/deno run -N -R=node_modules -W=node_modules --node-modules-dir=auto jsr:@pydantic/mcp-run-python stdio
